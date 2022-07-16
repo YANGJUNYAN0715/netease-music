6189edad4a893ae37b3bf6cb254b7b93638a537b
Junyan Yang <1310204834@qq.com>
2022-07-16 13:55:42 +0800

    2022.7.16

--- src/components/home/SwiperTop.vue
+++ src/components/home/SwiperTop.vue
@@ -1,32 +1,44 @@
 <template>
   <van-swipe :autoplay="3000" lazy-render>
-    <van-swipe-item v-for="image in images" :key="image">
-      <img :src="image" />
+    <van-swipe-item v-for="image in state.images" :key="image">
+      <img :src="image.pic" />
     </van-swipe-item>
   </van-swipe>
 </template>
 <script>
+import axios from "axios";
+import { reactive, onMounted } from "vue";
 export default {
   setup() {
-    const images = [
-      "https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg",
-      "https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg",
-    ];
-    return { images };
+    const state = reactive({
+      images: [
+        "https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg",
+        "https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg",
+      ],
+    });
+    onMounted(() => {
+      axios.get("http://localhost:3000/banner?type=2").then((res) => {
+        state.images = res.data.banners;
+      });
+    });
+    return { state };
   },
 };
 </script>
 <style lang="less" scoped>
 .van-swipe {
   width: 100%;
-  padding: 10px;
-  .van-swipe__track {
-    .van-swipe-item {
-      img {
-        width: 100%;
-        height: 100px;
-      }
+  height: 150px;
+  .van-swipe-item {
+    padding: 0 0.2rem;
+    img {
+      width: 100%;
+      height: 150px;
+      border-radius: 0.2rem;
     }
+  }
+  .van-swipe__indicator--active {
+    background-color: rgb(219, 130, 130);
   }
 }
 </style>
